48b2e73da0e615ea7653ea0edce3476cbf429c8d
wakewakame <0214.taiki@gmail.com>
2019-09-19 18:12:52 +0900

    update HydrangeaJS library

--- src/lib/HydrangeaJS/src/main.js
+++ src/lib/HydrangeaJS/src/main.js
@@ -1,9 +1,11 @@
 import { Graphics } from "./lib/graphics/graphics.js";
 import { GLCore } from "./lib/gl_core/gl_core.js";
+import { GLMath } from "./lib/utils/gl_math.js";
 
 export const HydrangeaJS = {
 	Graphics: Graphics,
 	GLCore: GLCore,
+	GLMath: GLMath,
 };
 
 window.HydrangeaJS = HydrangeaJS;
