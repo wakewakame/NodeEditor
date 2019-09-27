11f96b3ab1e57dd94995f87acae6feaf52df9f01
wakewakame <0214.taiki@gmail.com>
2019-09-27 19:25:10 +0900

    improved some points

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { HydrangeaJS } from "./lib/HydrangeaJS/src/main.js";
 import { RootComponent } from "./lib/Node/Component/root_component.js";
 import { DraggableComponent } from "./lib/Node/Component/draggable_component.js";
 import { FrameNode, TextureNode, ShaderNode } from "./lib/Node/NodeComponent/frame.js";
+import { Node, NodeParam } from "./lib/Node/Component/node_component.js";
 
 window.HydrangeaJS = HydrangeaJS;
 window.RootComponent = RootComponent;
@@ -9,3 +10,5 @@ window.DraggableComponent = DraggableComponent;
 window.FrameNode = FrameNode;
 window.TextureNode = TextureNode;
 window.ShaderNode = ShaderNode;
+window.Node = Node;
+window.NodeParam = NodeParam;
