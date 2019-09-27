d0f4e52eed688e55d3e6e8aeb855ddf8dff46ec8
wakewakame <0214.taiki@gmail.com>
2019-09-27 19:17:44 +0900

    improve some points

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { Applet } from "./lib/Node/node.js";
+import { HydrangeaJS } from "./lib/HydrangeaJS/src/main.js";
+import { RootComponent } from "./lib/Node/Component/root_component.js";
+import { DraggableComponent } from "./lib/Node/Component/draggable_component.js";
+import { FrameNode, TextureNode, ShaderNode } from "./lib/Node/NodeComponent/frame.js";
 
-export const Node = {
-	Applet: Applet,
-};
-
-window.Node = Node;
+window.HydrangeaJS = HydrangeaJS;
+window.RootComponent = RootComponent;
+window.DraggableComponent = DraggableComponent;
+window.FrameNode = FrameNode;
+window.TextureNode = TextureNode;
+window.ShaderNode = ShaderNode;
